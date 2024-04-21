53f2bfcdfdbdb565c211590ee7a7998226464cd7
nkansah wireko-brobbey <wirekobrobbeyofficial@gmail.com>
2024-04-21 16:41:35 -0500

    Add Socket.IO configuration to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,11 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+
+const socketConfig: SocketIoConfig = {url: 'http://localhost:4000', options: {}}
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), importProvidersFrom(SocketIoModule.forRoot(socketConfig))]
 };
